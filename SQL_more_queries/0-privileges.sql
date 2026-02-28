dc6caa5678c7f3066a0231bbebfb43e3f45252d0
David Magana <regulardavidluiz@gmail.com>
2026-02-28 00:33:03 -0500

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,3 +2,5 @@
 #!/bin/bash
 mysql -hlocalhost -uroot -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
 mysql -hlocalhost -uroot -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+chmod +x list_privileges.sh
+./list_privileges.sh
