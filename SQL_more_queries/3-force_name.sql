9d0394e2eb5080f24719868c3dc6ea6c7bb7c658
David Magana <regulardavidluiz@gmail.com>
2026-02-28 00:39:56 -0500

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- My comment
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
